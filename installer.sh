cfc06ce8fb54190fdf7177a52929c4164306e06c
German Antsiferov <dxdy@bk.ru>
2016-02-11 12:15:17 +0300

    Add function Ask to installer.sh

--- installer.sh
+++ installer.sh
@@ -3,13 +3,42 @@
 log()  { printf "%b\n" "$*"; }
 fail() { log "\nERROR: $*\n" ; exit 1 ; }
 
+# get from https://gist.github.com/davejamesmiller/1965569
+ask() 
+{
+  while true; do
+
+    if [ "${2:-}" = "Y" ]; then
+      prompt="Y/n"
+      default=Y
+    elif [ "${2:-}" = "N" ]; then
+      prompt="y/N"
+      default=N
+    else
+      prompt="y/n"
+      default=
+    fi
+
+    # Ask the question - use /dev/tty in case stdin is redirected from somewhere else
+    read -p "$1 [$prompt] " REPLY </dev/tty
+
+    # Default?
+    if [ -z "$REPLY" ]; then
+      REPLY=$default
+    fi
+
+    # Check if the reply is valid
+    case "$REPLY" in
+      Y*|y*) return 0 ;;
+      N*|n*) return 1 ;;
+    esac
+
+  done
+}
+
 sublime_package_manager_install()
 {
-  printf "%s\n" "The sublime package manager is installed on the PC? Y/N"
-  read -r answer
-  
-  if [ "$answer" = "N" ] || [ "$answer" = "n" ]
-  then
+  if ! ask "The sublime package manager is installed on the PC?", N; then
     fail "You should install sublime package manager. See https://packagecontrol.io/installation"
   fi 
 }
